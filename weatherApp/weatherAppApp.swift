0fa20ecda789058e5f22698dd28d2bf893ab1f09
Suneetha Nallamotu <suneetha_n@hotmail.com>
2023-02-09 14:32:53 -0600

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Suneetha Nallamotu on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
